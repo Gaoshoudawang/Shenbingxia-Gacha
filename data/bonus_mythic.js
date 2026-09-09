136d6b9cb085e48127dc2907c57220115acd94e9
Gaoshoudawang <226536392+Gaoshoudawang@users.noreply.github.com>
2026-09-09 19:27:46 +0800

    Add files via upload

---
+++ data/bonus_mythic.js
@@ -0,0 +1,10 @@
+window.BONUS_MYTHIC_POOL = [
+  {name: '传说女帝', quality: 'red', image: '../images/bonus/mythic/传说女帝.png', weight: 1.0},
+  {name: '王者之魄-传说光耀', quality: 'red', image: '../images/bonus/mythic/王者之魄-传说光耀.png', weight: 2.5},
+  {name: '王者之翼-传说夜影', quality: 'red', image: '../images/bonus/mythic/王者之翼-传说夜影.png', weight: 2.5},
+  {name: '王者之翼', quality: 'gold', image: '../images/bonus/mythic/王者之翼.png', weight: 1.5},
+  {name: '王者之魄', quality: 'gold', image: '../images/bonus/mythic/王者之魄.png', weight: 1.5},
+  {name: '神兵匣积分×24', quality: 'purple', image: '../images/bonus/mythic/神兵匣积分.png', weight: 15.0},
+  {name: '神兵匣积分×20', quality: 'purple', image: '../images/bonus/mythic/神兵匣积分.png', weight: 30.0},
+  {name: '神兵匣积分×18', quality: 'purple', image: '../images/bonus/mythic/神兵匣积分.png', weight: 46.0},
+];
